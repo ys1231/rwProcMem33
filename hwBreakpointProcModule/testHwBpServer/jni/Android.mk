85c4f594f047ccc0e20e3b8885d488e951985315
iyueforlinux <ys1231@126.com>
2024-02-06 11:53:46 +0800

    fix(build): :art: 优化编译输出不同abi对应名称

--- hwBreakpointProcModule/testHwBpServer/jni/Android.mk
+++ hwBreakpointProcModule/testHwBpServer/jni/Android.mk
@@ -6,7 +6,22 @@ LOCAL_CFLAGS += -fPIE
 LOCAL_CFLAGS += -fvisibility=hidden
 LOCAL_LDFLAGS += -fPIE -pie
 LOCAL_DISABLE_FATAL_LINKER_WARNINGS := true
-LOCAL_MODULE    := testHwBpServer.out
+
+# Set the output name based on the ABI
+ifeq ($(TARGET_ARCH_ABI),x86)
+    APP_OUTPUT_NAME := -x86-server
+endif
+ifeq ($(TARGET_ARCH_ABI),x86_64)
+    APP_OUTPUT_NAME := -x86_64-server
+endif
+ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
+    APP_OUTPUT_NAME := -armeabi_v7a-server
+endif
+ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
+    APP_OUTPUT_NAME := -arm64_v8a-server
+endif
+
+LOCAL_MODULE    := testHwBp$(APP_OUTPUT_NAME)
 LOCAL_SRC_FILES :=  ../main.cpp ../api.cpp ../Global.cpp  ../porthelp.cpp
 LOCAL_LDLIBS := -lz -llog
 include $(BUILD_EXECUTABLE)
